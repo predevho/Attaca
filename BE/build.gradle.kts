e198c4993557bcc381bd1da4cdfe01bc247857b4
predevho <dlagusgh7894@gmail.com>
2026-07-15 11:11:13 +0900

    feat: 런타임 MySQL 데이터소스 구성 및 테스트 H2 프로파일 분리

--- BE/build.gradle.kts
+++ BE/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
